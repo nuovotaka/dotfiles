7eab22142dffc269fa2997470dca2b32c755a664
Takahiro Natsume <tkn.infomail@gmail.com>
2026-05-02 14:32:23 +0900

    change nvim plugin

--- home-manager/config/nvim/init.lua
+++ home-manager/config/nvim/init.lua
@@ -1,11 +1,2 @@
-require('craftzdog.base')
-require('craftzdog.highlights')
-require('craftzdog.maps')
-require('craftzdog.plugins')
-
-local has = vim.fn.has
-local is_mac = has "macunix"
-
-if is_mac then
-  require('craftzdog.macos')
-end
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
